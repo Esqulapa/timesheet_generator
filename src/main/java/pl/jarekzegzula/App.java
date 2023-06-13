7433a61b227ba7146299fa4d170d0ea2083d4d64
Esqulapa <jaroslaw.zegzula@gmail.com>
2023-06-13 23:38:54 +0200

    added lots of classes and functions

---
+++ src/main/java/pl/jarekzegzula/App.java
@@ -0,0 +1,15 @@
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.temporal.TemporalAdjusters;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
