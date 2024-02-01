f39a88c94c610a97006d3b2b52b7f14aab7dd72d
Esqulapa <jaroslaw.zegzula@gmail.com>
2024-02-01 17:41:53 +0100

    little cleanup

--- src/main/java/pl/jarekzegzula/WorkingDays.java
+++ src/main/java/pl/jarekzegzula/WorkingDays.java
@@ -5,18 +5,15 @@ import java.util.List;
 import java.util.Objects;
 
 public record WorkingDays(List<DayOfWeek> workingDays) {
-
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        WorkingDays that = (WorkingDays) o;
-        return Objects.equals(workingDays, that.workingDays);
+ @Override
+ public boolean equals(Object o) {
+     if (this == o) return true;
+     if (o == null || getClass() != o.getClass()) return false;
+     WorkingDays that = (WorkingDays) o;
+     return Objects.equals(workingDays, that.workingDays);
     }
-
     @Override
     public int hashCode() {
-        return Objects.hash(workingDays);
+     return Objects.hash(workingDays);
     }
 }
